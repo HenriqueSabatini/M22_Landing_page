2bc076589fc96f8798726db570af58afd378007c
Henrique Sabatini <henriquesabatini2016@gmail.com>
2024-04-05 17:35:10 -0300

    criando a contagem

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("ola H");
+AOS.init();
+
+const dataDoEvento = new Date("Jul 23, 2024 23:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaaAteOEvento % diaEmMs) / horaEmMs);
+    const minutoAteOEvento = Math.floor((distanciaaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutoAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000);
